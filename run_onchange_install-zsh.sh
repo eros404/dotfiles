eda5e80f568663cbf3753d7e6b330e0fdb6ba50c
Louis Morando <contact@louis-morando.fr>
2025-02-10 22:06:23 +0100

    fix

--- run_onchange_install-zsh.sh
+++ run_onchange_install-zsh.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+if [[ -z $ZSH ]]; then
 sudo dnf install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
