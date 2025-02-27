d31d7851c0f18dfda7162a3b3af63ac6ff1457dc
Louis Morando <contact@louis-morando.fr>
2025-02-27 22:58:00 +0100

    neovim and ignore all community files

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -7,7 +7,6 @@ return {
 	"AstroNvim/astrocommunity",
 	{ import = "astrocommunity.pack.lua" },
 	-- import/override with your plugins folder
-	{ import = "astrocommunity.pack.cs" },
 	{ import = "astrocommunity.pack.chezmoi" },
 	{ import = "astrocommunity.pack.bash" },
 	{ import = "astrocommunity.colorscheme.catppuccin" },
--- run_onchange.sh
+++ run_onchange.sh
@@ -2,29 +2,34 @@
 
 # Install ZSH
 if [ -z "$ZSH" ]; then
-  sudo dnf install zsh
+  sudo dnf install -y zsh
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 # Install Starship
 if ! which starship; then
   sudo dnf copr enable atim/starship
-  sudo dnf install starship
+  sudo dnf install -y starship
 fi
 
 # Install lsd
 if ! which lsd; then
-  sudo dnf install lsd
+  sudo dnf install -y lsd
   wget -P "$HOME/.config/lsd" https://raw.githubusercontent.com/catppuccin/lsd/refs/heads/main/themes/catppuccin-macchiato/colors.yaml
 fi
 
 # Install bat
 if ! which bat; then
-  sudo dnf install bat
+  sudo dnf install -y bat
   mkdir -p "$(bat --config-dir)/themes"
   wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Latte.tmTheme
   wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Frappe.tmTheme
   wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Macchiato.tmTheme
   wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Mocha.tmTheme
   bat cache --build
+fi
+
+# Install NeoVim
+if ! which nvim; then
+  sudo dnf install -y neovim python3-neovim
 fi
