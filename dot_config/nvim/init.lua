b576298de812c6c096fd825106e12c71c2147e33
Louis Morando <contact@louis-morando.fr>
2026-05-01 09:33:50 +0200

    Add .config/nvim/init.lua

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,11 @@
+vim.o.number = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.hlsearch = false
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.showmode = false
+vim.o.signcolumn = 'yes'
+
+vim.opt.clipboard = 'unnamedplus'
+
