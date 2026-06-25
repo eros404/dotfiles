abc319f8e729fe1f1135d9485ed3614ef116bbb3
Louis Morando <contact@louis-morando.fr>
2026-06-25 18:51:14 +0200

    Remove Pictures/face.png Remove .config/niri/config.kdl Remove .config/niri/dms/binds.kdl Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -8,15 +8,4 @@ vim.o.showmode = false
 vim.o.signcolumn = 'yes'
 
 vim.opt.clipboard = 'unnamedplus'
-vim.g.clipboard = {
-	name = "dms cl (Dank clipboard)",
-  copy = {
-  	["+"] = 'dms cl copy -t text/plain',
-    ["*"] = 'dms cl copy -t text/plain',
-  },
-  paste = {
-    ["+"] = 'dms cl paste',
-		["*"] = 'dms cl paste',
-  },
-  cache_enabled = true
-}
+
