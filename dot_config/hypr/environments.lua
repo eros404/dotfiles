55e512159d7ca5da58d3a535a4ee99a56fca3034
Louis Morando <contact@louis-morando.fr>
2026-06-25 18:48:06 +0200

    Add .config/hypr/environments.lua

---
+++ dot_config/hypr/environments.lua
@@ -0,0 +1,12 @@
+hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
+hl.env("XDG_SESSION_TYPE", "wayland")
+hl.env("XDG_SESSION_DESKTOP", "Hyprland")
+
+hl.env("QT_QPA_PLATFORM", "wayland")
+hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
+hl.env("QT_QPA_PLATFORMTHEME", "gtk3")
+hl.env("QT_QPA_PLATFORMTHEME_QT6", "gtk3")
+
+hl.env("XCURSOR_THEME", "capitaine-cursors")
+hl.env("XCURSOR_SIZE", "32")
+
