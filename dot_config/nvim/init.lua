5ce46fa974dda5a2e8d4c7a23cfc1327e05d94cb
Louis Morando <contact@louis-morando.fr>
2026-07-20 09:52:25 +0200

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -16,7 +16,27 @@ vim.pack.add({
 		name = "rose-pine",
 	},
 })
+
+vim.cmd("set termguicolors")
+
 require("rose-pine").setup()
 vim.cmd("colorscheme rose-pine")
 
-vim.cmd("highlight Normal guibg=none")
+vim.cmd("highlight Normal guibg=none ctermbg=none")
+vim.cmd("highlight NonText guibg=none ctermbg=none")
+vim.cmd("highlight SignColumn guibg=none ctermbg=none")
+vim.cmd("highlight EndOfBuffer guibg=none ctermbg=none")
+vim.cmd("highlight MsgArea guibg=none ctermbg=none")
+vim.cmd("highlight LineNr guibg=none ctermbg=none")
+vim.cmd("highlight CursorLineNr guibg=none ctermbg=none")
+vim.cmd("highlight StatusLine guibg=none ctermbg=none")
+vim.cmd("highlight StatusLineNC guibg=none ctermbg=none")
+vim.cmd("highlight TabLine guibg=none ctermbg=none")
+vim.cmd("highlight TabLineFill guibg=none ctermbg=none")
+vim.cmd("highlight TabLineSel guibg=none ctermbg=none")
+vim.cmd("highlight VertSplit guibg=none ctermbg=none")
+vim.cmd("highlight FloatBorder guibg=none ctermbg=none")
+vim.cmd("highlight NormalFloat guibg=none ctermbg=none")
+vim.cmd("highlight Pmenu guibg=none ctermbg=none")
+vim.cmd("highlight PmenuSel guibg=none ctermbg=none")
+
