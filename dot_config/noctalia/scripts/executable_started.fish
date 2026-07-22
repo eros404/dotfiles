63876beea01eb6c45575d9dd81c2befcbe850c77
Louis Morando <contact@louis-morando.fr>
2026-07-22 18:21:36 +0200

    Update .config/noctalia/bar.toml Update .config/noctalia/hooks.toml Add .config/noctalia/scripts/started.fish Add .config/noctalia/scripts/theme_mode_changed.fish Update .config/noctalia/settings.toml Update .config/noctalia/shell.toml

---
+++ dot_config/noctalia/scripts/executable_started.fish
@@ -0,0 +1,7 @@
+#!/usr/bin/fish
+
+set NOCTALIA_THEME_MODE $(noctalia msg theme-mode-get)
+
+gsettings set org.gnome.desktop.interface color-scheme "prefer-$NOCTALIA_THEME_MODE"
+noctalia msg wallpaper-set "$HOME/Pictures/background-$NOCTALIA_THEME_MODE.png"
+
---
+++ dot_config/noctalia/scripts/executable_theme_mode_changed.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/fish
+
+gsettings set org.gnome.desktop.interface color-scheme "prefer-$NOCTALIA_THEME_MODE"
+noctalia msg wallpaper-set "$HOME/Pictures/background-$NOCTALIA_THEME_MODE.png"
+
